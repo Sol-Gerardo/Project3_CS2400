0e619051b71f1b4879debb5e507953a4d95cd017
Gerardo Solis <56048406+Sol-Jerry@users.noreply.github.com>
2022-04-06 20:27:38 -0700

    Tasks 3 & 4

---
+++ EmptyTreeException.java
@@ -0,0 +1,14 @@
+package Project_3.atHome;
+
+public class EmptyTreeException extends RuntimeException
+{
+	public EmptyTreeException()
+	{
+		this(null);
+	} // end default constructor
+
+	public EmptyTreeException(String message)
+	{
+		super(message);
+	} // end constructor
+} // end EmptyTreeException
---
+++ TreeInterface.java
@@ -0,0 +1,10 @@
+package Project_3.atHome;
+
+public interface TreeInterface<T>
+{
+   public T getRootData();
+   public int getHeight();
+   public int getNumberOfNodes();
+   public boolean isEmpty();
+   public void clear();
+} // end TreeInterface
